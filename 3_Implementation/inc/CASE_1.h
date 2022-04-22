65401e4ca5bdb4e5242375ea2c292ca18c57064f
kommalapatisowmya <101501471+kommalapatisowmya@users.noreply.github.com>
2022-04-22 22:57:59 +0530

    Add files via upload

---
+++ 3_Implementation/inc/CASE_1.h
@@ -0,0 +1,54 @@
+#ifndef __CASE_1_H__
+#define __CASE_1_H__
+
+#ifndef __AVR_ATmega328__
+#define __AVR_ATmega328__
+#endif
+
+
+/**
+ * Macro Definitions
+ */
+
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+#define LED_PORT (PORTD)    /**< LED Port Number */
+#define LED_PIN  (PORTD2)   /**< LED Pin number  */
+#define BUTTON_SENSOR  (PORTD0)   /**< Port for Button Sensor  */
+#define TEMP_SENSOR  (PORTD1)   /**< Port for Temperature Sensor   */
+
+/**
+ * Include files
+ */ 
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Initialize all the Peripherals and pin configurations
+ * 
+ */
+void peripheral_init(void);
+
+/**
+ * @brief Function to turn LED on
+ * 
+ */
+void TurnLED_ON();
+
+/**
+ * @brief Function to turn LED off
+ * 
+ */
+void TurnLED_OFF();
+
+/**
+ * @brief Activity 1 to Turn ON LED if switches for Button sensor and Heator sensor are pressed 
+ * 
+ */
+int CASE_1_LED(void);
+
+#endif /* __CASE_1_H__ */
